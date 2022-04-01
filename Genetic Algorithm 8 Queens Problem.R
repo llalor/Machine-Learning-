cb143d1bc05201a756d464899509b009d615c68e
Luca Lalor <72799772+llalor@users.noreply.github.com>
2022-04-01 09:15:55 -0600

    Update Genetic Algorithm 8 Queens Problem.R
    https://en.wikipedia.org/wiki/Eight_queens_puzzle#:~:text=The%20eight%20queens%20puzzle%20is,in%20the%20mid%2D19th%20century.

--- Genetic Algorithm 8 Queens Problem.R
+++ Genetic Algorithm 8 Queens Problem.R
@@ -1,11 +1,11 @@
 rm(list = ls()) #cleaning the workspace
 
 
-##Step 1: The Problem
+## Step 1: The Problem
 if (!require('NMOF')) install.packages('NMOF');library("NMOF")
 set.seed(134577)
 
-##Step 2: Representing a solution #
+## Step 2: Representing a solution #
 N <- 8 ## board size 
 p <- sample.int(N) ## a random solution 
 data.frame(row = 1:N, column = p)
@@ -25,7 +25,7 @@ print_board <- function(p, q.char = "Q", sep = " ") {
 }
 print_board(p) ##phenotype configuration??
 
-##Step 3: Evaluating a solution ##fitness function
+## Step 3: Evaluating a solution ##fitness function
 mat <- array(NA, dim = c(N,N)) ## diagonals
  for (r in 1:N)
   for (c in 1:N)
@@ -48,7 +48,7 @@ n_attacks <- function(p) {
 }
  n_attacks(p)
 
- ##Step 4: Changing a solution ##Mutation 
+ ## Step 4: Changing a solution ##Mutation 
  
  neighbour <- function(p) {
    step <- 3
@@ -65,20 +65,20 @@ n_attacks <- function(p) {
   print_board(p <- neighbour(p)) #Move queen
   print_board(p <- neighbour(p))
   
-  ##Step 5: Solving the model
+  ## Step 5: Solving the model
   p0 <- rep(1, N) ## or a random initial solution: p0 <- sample.int(N)
   print_board(p0)
   
-  ##Genetic algorithm
+  ## Genetic algorithm
   sol <- LSopt(n_attacks, list(x0 = p0,neighbour = neighbour,
                                printBar = FALSE, nS = 10000)) 
   print_board(sol$xbest) 
  
-   ##Threshold Accepting
+   ## Threshold Accepting
    sol <- TAopt(n_attacks, list(x0 = p0, neighbour = neighbour, printBar = FALSE,nS = 1000))
   print_board(sol$xbest)
   
-  #simulated annealing
+  ## simulated annealing
   sol <- SAopt(n_attacks, list(x0 = p0, neighbour = neighbour, 
                                                      printBar = FALSE,nS = 1000)) 
   print_board(sol$xbest)
